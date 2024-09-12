d5981bad829c1d37e58ead5bfd54e4b63f776381
estebanrdavidc <estebanrdavidc@gmail.com>
2024-09-12 17:16:09 -0500

    Update Juego.h

--- Juego.h
+++ Juego.h
@@ -1,6 +1,8 @@
 #ifndef JUEGO_H
 #define JUEGO_H
 
+#include <string>
+
 class Jugador {
 public:
     Jugador();
@@ -8,6 +10,8 @@ public:
     void sumarPuntosTotales(int puntosGanados);
     void resetearPuntosRonda();
     void resetearPuntosTotales();
+    void cargarEstado(std::istream& input);
+    void guardarEstado(std::ostream& output) const;
 
 private:
     int puntosTotales;
@@ -18,14 +22,18 @@ class Juego {
 public:
     Juego();
     void jugar();
+    void guardarPartida(const std::string& nombreArchivo) const;
+    void cargarPartida(const std::string& nombreArchivo);
 
 private:
     void jugarRonda();
     int lanzarDado() const;
     void anunciarGanador() const;
+    void continuarOguardar();
 
     Jugador jugador1, jugador2;
     int ronda;
 };
 
 #endif // JUEGO_H
+
