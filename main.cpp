d7e3bbfa179d4a724ea0f3c6d679cfa38b5fafd8
estebanrdavidc <estebanrdavidc@gmail.com>
2024-09-12 17:15:19 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
+#include <iostream>
 #include "Juego.h"
+#include "Juego.cpp"
 
 int main() {
     Juego juego;
+    int opcion;
+    std::cout << "¿Desea iniciar un nuevo juego (1) o cargar una partida existente (2)? ";
+    std::cin >> opcion;
+    
+    if (opcion == 2) {
+        std::string nombreArchivo;
+        std::cout << "Ingrese el nombre del archivo para cargar: ";
+        std::cin >> nombreArchivo;
+        juego.cargarPartida(nombreArchivo);
+    }
+    
     juego.jugar();
+    
     return 0;
 }
